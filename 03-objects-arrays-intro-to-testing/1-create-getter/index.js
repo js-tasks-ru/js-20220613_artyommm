135ba35a8c25b4a70f64b3ae0ead08a2d83edcbf
artyommm <bahareff.artyom@yandex.ru>
2022-06-21 10:25:15 +0500

    03.1 create getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,15 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  const fields = path.split('.');
+  return (product) => {
+    let result = product;
+    for (const field of fields) {
+      result = result[field]; //try to get field
+      if (!result) { //if get undefined -> return undefined
+        return result;
+      }
+    }
+    return result;
+  };
 }
